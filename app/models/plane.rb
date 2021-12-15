bbf36d1e44866f39444cbb3204926174096cbd07
Elsa Lebas <lebas.elsa@gmail.com>
2021-12-15 15:05:58 +0100

    generate plane model with validations and association

---
+++ app/models/plane.rb
@@ -0,0 +1,9 @@
+class Plane < ApplicationRecord
+  CAPACITIES = ["1", "2", "3", "4", "5-6", "7-9", "10+"]
+  belongs_to :user
+  validates :plane_name, location:, presence: true
+  validates :plane_type, inclusion: { in: ["single-engined", "twin-engined"] }
+  validates :capacity, inclusion: { in: CAPACITIES }
+  validates :price, numericality: { only_integer: true, greater_than: 0 }
+  validates :available, inclusion: [true, false] #could also be exclusion: [nil]
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_131723) do
+ActiveRecord::Schema.define(version: 2021_12_15_135152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "planes", force: :cascade do |t|
+    t.string "plane_name"
+    t.string "plane_type"
+    t.string "location"
+    t.string "capacity"
+    t.integer "price"
+    t.boolean "available"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_planes_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -31,4 +44,5 @@ ActiveRecord::Schema.define(version: 2021_12_13_131723) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "planes", "users"
 end

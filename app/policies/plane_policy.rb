74ee006a5fc54d54d800fd5f6c5040d210dae61a
Elsa Lebas <lebas.elsa@gmail.com>
2021-12-17 16:05:01 +0100

    initi pages controller and home view

---
+++ app/policies/plane_policy.rb
@@ -0,0 +1,29 @@
+class PlanePolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    record.user == user # record = the plane instance passed to the `authorize` method in controller and user:   current_user
+  end
+
+  def destroy?
+    record.user == user
+  end
+  
+end
+
+
+
+
+

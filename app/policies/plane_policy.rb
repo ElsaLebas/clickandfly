7516a0213bb423f11b85659ca3a261ffc6201ce8
Elsa Lebas <lebas.elsa@gmail.com>
2021-12-17 17:59:13 +0100

    pundit setup for authorization

--- app/policies/plane_policy.rb
+++ app/policies/plane_policy.rb
@@ -14,7 +14,7 @@ class PlanePolicy < ApplicationPolicy
   end
 
   def update?
-    record.user == user # record = the plane instance passed to the `authorize` method in controller and user:   current_user
+    record.user == user # record = the plane instance passed to the `authorize` method in controller and user => current_user
   end
 
   def destroy?
